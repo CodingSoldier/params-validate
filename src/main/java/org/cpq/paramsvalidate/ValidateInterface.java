573269fb71abd01279101b6c4133f6320d4436c6
陈丕迁 <237053176@qq.com>
2018-05-23 17:11:06 +0800

    项目第一版

---
+++ src/main/java/org/cpq/paramsvalidate/ValidateInterface.java
@@ -0,0 +1,36 @@
+package org.cpq.paramsvalidate;
+
+import org.cpq.paramsvalidate.bean.ResultValidate;
+import org.cpq.paramsvalidate.bean.ValidateConfig;
+
+import java.util.Map;
+
+public interface ValidateInterface {
+    /**
+     * 返回json文件基础路径。init.json文件必须放在此目录下
+     * @return json文件基础路径
+     */
+    String basePath();
+
+    /**
+     * 参数校验未通过
+     * @param resultValidate
+     * @return 返回给客户端的数据
+     */
+    Object validateNotPass(ResultValidate resultValidate);
+
+    /**
+     * 获取缓存中的校验规则
+     * @param validateConfig
+     * @return  校验规则
+     */
+    Map<String, Object> getCache(ValidateConfig validateConfig);
+
+    /**
+     * 设置校验规则到缓存中
+     * @param validateConfig
+     * @param json 校验规则
+     */
+    void setCache(ValidateConfig validateConfig, Map<String, Object> json);
+
+}
